4b936cb41da8267d81fda949abc9b8e374978244
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2022-05-10 09:31:46 -0300

    feat: method to withdraw

--- banco/main.go
+++ banco/main.go
@@ -9,38 +9,58 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso!"
+	}
+	return "Saldo insuficiente"
+}
+
 func main() {
+	contaDaSilvia := ContaCorrente{}
+	contaDaSilvia.titular = "Silvia"
+	contaDaSilvia.saldo = 500
+
+	fmt.Println(contaDaSilvia.saldo)
+
+	fmt.Println(contaDaSilvia.Sacar(100))
+	fmt.Println(contaDaSilvia.Sacar(-100))
+
+	fmt.Println(contaDaSilvia.saldo)
 
 	// var contaDoEduardo ContaCorrente = ContaCorrente{}
 
-	contaGuilherme := ContaCorrente{
-		titular: "Guilherme",
-		saldo:   123.5,
-	}
+	// contaGuilherme := ContaCorrente{
+	// 	titular: "Guilherme",
+	// 	saldo:   123.5,
+	// }
 
-	contaDoEduardo := ContaCorrente{
-		titular:       "Eduardo",
-		numeroAgencia: 125,
-		numeroConta:   121121,
-		saldo:         122.4,
-	}
+	// contaDoEduardo := ContaCorrente{
+	// 	titular:       "Eduardo",
+	// 	numeroAgencia: 125,
+	// 	numeroConta:   121121,
+	// 	saldo:         122.4,
+	// }
 
-	contaDaBruna := ContaCorrente{
-		"Bruna",
-		222,
-		1212455,
-		522.4,
-	}
+	// contaDaBruna := ContaCorrente{
+	// 	"Bruna",
+	// 	222,
+	// 	1212455,
+	// 	522.4,
+	// }
 
-	// var contaDaCris *ContaCorrente
-	// contaDaCris = new(ContaCorrente)
+	// // var contaDaCris *ContaCorrente
+	// // contaDaCris = new(ContaCorrente)
 
-	contaDaCris := new(ContaCorrente)
-	contaDaCris.titular = "Cris"
+	// contaDaCris := new(ContaCorrente)
+	// contaDaCris.titular = "Cris"
 
-	fmt.Println(contaDoEduardo)
-	fmt.Println(contaDaBruna)
-	fmt.Println(contaGuilherme)
-	fmt.Println(*contaDaCris)
+	// fmt.Println(contaDoEduardo)
+	// fmt.Println(contaDaBruna)
+	// fmt.Println(contaGuilherme)
+	// fmt.Println(*contaDaCris)
 
 }
