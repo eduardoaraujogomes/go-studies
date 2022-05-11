31abaf291258f22390bdc5d59b31bb98931d22c6
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2022-05-11 16:10:41 -0300

    feat: create func to return balance

--- banco/contas/contaCorrente.go
+++ banco/contas/contaCorrente.go
@@ -1,37 +1,43 @@
 package contas
 
+import "banco/clientes"
+
 type ContaCorrente struct {
-	Titular       string
+	Titular       clientes.Titular
 	NumeroAgencia int
 	NumeroConta   int
-	Saldo         float64
+	saldo         float64
 }
 
 func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.Saldo
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
 
 	if podeSacar {
-		c.Saldo -= valorDoSaque
+		c.saldo -= valorDoSaque
 		return "Saque realizado com sucesso!"
 	}
-	return "Saldo insuficiente"
+	return "saldo insuficiente"
 }
 
 func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
 	if valorDoDeposito > 0 {
-		c.Saldo += valorDoDeposito
-		return "Deposito realizado com sucesso. Saldo atual: ", c.Saldo
+		c.saldo += valorDoDeposito
+		return "Deposito realizado com sucesso. saldo atual: ", c.saldo
 	}
-	return "Valor do depósito menor que zero. Saldo atual: ", c.Saldo
+	return "Valor do depósito menor que zero. saldo atual: ", c.saldo
 }
 
 func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
-	if valorDaTransferencia < c.Saldo && valorDaTransferencia > 0 {
-		c.Saldo -= valorDaTransferencia
+	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
+		c.saldo -= valorDaTransferencia
 		contaDestino.Depositar(valorDaTransferencia)
 		return true
 	}
 	return false
+}
+
+func (c *ContaCorrente) ObterSaldo() float64 {
+	return c.saldo
 }
 
 // função variádica
--- banco/main.go
+++ banco/main.go
@@ -1,79 +1,87 @@
 package main
 
 import (
+	"banco/contas"
 	"fmt"
-
-	"./contas"
 )
 
 func main() {
+	contaExemplo := contas.ContaCorrente{}
+	contaExemplo.Depositar(100)
 
-	contaDaSilvia := contas.ContaCorrente{Titular: "Silvia", Saldo: 300}
-	contaDoGustavo := contas.ContaCorrente{Titular: "Gustavo", Saldo: 100}
-
-	status := contaDaSilvia.Transferir(200, &contaDoGustavo)
-
-	fmt.Println(status)
-	fmt.Println(contaDaSilvia)
-	fmt.Println(contaDoGustavo)
-
-	// contaDaSilvia := ContaCorrente{}
-	// contaDaSilvia.titular = "Silvia"
-	// contaDaSilvia.saldo = 500
-
-	// fmt.Println(contaDaSilvia.saldo)
-
-	// fmt.Println(contaDaSilvia.Sacar(100))
-	// fmt.Println(contaDaSilvia.Sacar(-100))
-
-	// fmt.Println(contaDaSilvia.saldo)
-
-	// fmt.Println(contaDaSilvia.Depositar(100))
-	// fmt.Println(contaDaSilvia.Depositar(-100))
-
-	// status, valor := contaDaSilvia.Depositar(200)
-	// fmt.Println(status, valor)
-
-	// fmt.Println(contaDaSilvia.saldo)
-
-	//função variádica
-	// numeros := []int{1, 2, 3, 4, 5, 6}
-	// fmt.Println(Somando(numeros...))
-	// fmt.Println(Somando(1))
-	// fmt.Println(Somando(1, 1))
-	// fmt.Println(Somando(1, 1, 1))
-	// fmt.Println(Somando(1, 1, 2, 4))
-
-	// var contaDoEduardo ContaCorrente = ContaCorrente{}
-
-	// contaGuilherme := ContaCorrente{
-	// 	titular: "Guilherme",
-	// 	saldo:   123.5,
-	// }
-
-	// contaDoEduardo := ContaCorrente{
-	// 	titular:       "Eduardo",
-	// 	numeroAgencia: 125,
-	// 	numeroConta:   121121,
-	// 	saldo:         122.4,
-	// }
-
-	// contaDaBruna := ContaCorrente{
-	// 	"Bruna",
-	// 	222,
-	// 	1212455,
-	// 	522.4,
-	// }
-
-	// // var contaDaCris *ContaCorrente
-	// // contaDaCris = new(ContaCorrente)
-
-	// contaDaCris := new(ContaCorrente)
-	// contaDaCris.titular = "Cris"
-
-	// fmt.Println(contaDoEduardo)
-	// fmt.Println(contaDaBruna)
-	// fmt.Println(contaGuilherme)
-	// fmt.Println(*contaDaCris)
+	fmt.Println(contaExemplo.ObterSaldo())
 
 }
+
+// clienteBruno := clientes.Titular{"Bruno", "123.111.123.11", "Desenvolvedor"}
+// contaDoBruno := contas.ContaCorrente{clienteBruno, 123, 121311, 100.0}
+
+// fmt.Println(contaDoBruno)
+
+// contaDaSilvia := contas.ContaCorrente{Titular: "Silvia", Saldo: 300}
+// contaDoGustavo := contas.ContaCorrente{Titular: "Gustavo", Saldo: 100}
+
+// status := contaDaSilvia.Transferir(200, &contaDoGustavo)
+
+// fmt.Println(status)
+// fmt.Println(contaDaSilvia)
+// fmt.Println(contaDoGustavo)
+
+// contaDaSilvia := ContaCorrente{}
+// contaDaSilvia.titular = "Silvia"
+// contaDaSilvia.saldo = 500
+
+// fmt.Println(contaDaSilvia.saldo)
+
+// fmt.Println(contaDaSilvia.Sacar(100))
+// fmt.Println(contaDaSilvia.Sacar(-100))
+
+// fmt.Println(contaDaSilvia.saldo)
+
+// fmt.Println(contaDaSilvia.Depositar(100))
+// fmt.Println(contaDaSilvia.Depositar(-100))
+
+// status, valor := contaDaSilvia.Depositar(200)
+// fmt.Println(status, valor)
+
+// fmt.Println(contaDaSilvia.saldo)
+
+//função variádica
+// numeros := []int{1, 2, 3, 4, 5, 6}
+// fmt.Println(Somando(numeros...))
+// fmt.Println(Somando(1))
+// fmt.Println(Somando(1, 1))
+// fmt.Println(Somando(1, 1, 1))
+// fmt.Println(Somando(1, 1, 2, 4))
+
+// var contaDoEduardo ContaCorrente = ContaCorrente{}
+
+// contaGuilherme := ContaCorrente{
+// 	titular: "Guilherme",
+// 	saldo:   123.5,
+// }
+
+// contaDoEduardo := ContaCorrente{
+// 	titular:       "Eduardo",
+// 	numeroAgencia: 125,
+// 	numeroConta:   121121,
+// 	saldo:         122.4,
+// }
+
+// contaDaBruna := ContaCorrente{
+// 	"Bruna",
+// 	222,
+// 	1212455,
+// 	522.4,
+// }
+
+// // var contaDaCris *ContaCorrente
+// // contaDaCris = new(ContaCorrente)
+
+// contaDaCris := new(ContaCorrente)
+// contaDaCris.titular = "Cris"
+
+// fmt.Println(contaDoEduardo)
+// fmt.Println(contaDaBruna)
+// fmt.Println(contaGuilherme)
+// fmt.Println(*contaDaCris)
